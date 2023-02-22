4ad099d45f81d64972405e482ba7f578f152a808
achingachris <achinga.chris@gmail.com>
2023-02-22 15:50:11 +0300

    adding script link

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,11 +2,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
         <NextScript />
+        {/* Custom JS files */}
+        <script
+          async
+          src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'
+        ></script>
+
+        {/* theme js */}
+        <script async src='/js/scripts.js'></script>
+
+        {/* startbootstrap forms */}
+        <script
+          async
+          src='https://cdn.startbootstrap.com/sb-forms-latest.js'
+        ></script>
       </body>
     </Html>
   )
