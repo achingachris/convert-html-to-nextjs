e15ebbc27a24624054ef1882cfb8baf60f762de7
achingachris <achinga.chris@gmail.com>
2023-02-22 16:43:25 +0300

    complete conversion to jsx

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,28 @@
+/* eslint-disable @next/next/google-font-display */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
-      <body>
+      <Head>
+        <script
+          async
+          src='https://use.fontawesome.com/releases/v6.1.0/js/all.js'
+          crossorigin='anonymous'
+        ></script>
+        {/* <!-- Google fonts--> */}
+        <link
+          href='https://fonts.googleapis.com/css?family=Montserrat:400,700'
+          rel='stylesheet'
+          type='text/css'
+        />
+        <link
+          href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic'
+          rel='stylesheet'
+          type='text/css'
+        />
+      </Head>
+      <body id='page-top'>
         <Main />
         <NextScript />
         {/* Custom JS files */}
